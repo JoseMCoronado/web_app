fbdf6c0ce2ebecb4d667bf15703631d74e1fd9b0
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-08-06 22:55:36 -0700

    Messy commit to save current work in progress, web_app semi operational.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #main.py
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Table, Column, Integer, String, Date, Float, ForeignKey, DateTime
+from sqlalchemy import *#Table, Column, Integer, String, Date, Float, ForeignKey, DateTime
 import os, sys
+import urllib2
+import requests
+import json
 
 
 app = Flask(__name__)
@@ -22,7 +25,7 @@ db = SQLAlchemy(app)
 import models
 # main.py
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def show_home_page():
     pastes = models.Paste.query.order_by("date desc").limit(10)
     if pastes == None:
@@ -57,8 +60,9 @@ def submit():
     text = text.encode("ascii", errors="ignore")
     poster = request.form["poster"]
     poster = poster.encode("ascii", errors="ignore")
+    value = request.form["value"]
 
-    paste = models.Paste(text, poster)
+    paste = models.Paste(text, poster, value)
 
     db.session.add(paste)
     db.session.commit()
@@ -70,19 +74,17 @@ def submit():
         raise Exception("No pastes in db")
     return render_template("home.html", pastes=pastes)
 
-@app.route("/<id>")
-def show(id):
-    pastes = models.Paste.query.filter_by(id=id)
-    prepaste = pastes.first()
-    pasteid = prepaste.id
-    pastecontent = prepaste.paste
+#@app.route("/<id>")
+#def show(id):
+#    pastes = models.Paste.query.filter_by(id=id)
+#    prepaste = pastes.first()
+#    pasteid = prepaste.id
+#    pastecontent = prepaste.paste
 
-    if prepaste == None:
-        raise Exception("No such paste by id %s" % id)
+#    if prepaste == None:
+#        raise Exception("No such paste by id %s" % id)
 
-    return render_template("paste.html", pasteid=pasteid, pastecontent=pastecontent)
-
-
+#    return render_template("paste.html", pasteid=pasteid, pastecontent=pastecontent)
 
 @app.route("/all")
 def showall():
@@ -91,5 +93,83 @@ def showall():
         raise Exception("No pastes in db")
     return render_template("allpastes.html", pastes=pastes)
 
+#api endpoints
+@app.route('/api/all')
+def api_all():
+  events = models.Paste.query.all()
+  return jsonify([p.serialize for p in events])
+
+@app.route('/api/<event_type>')
+def api_by_event_type(event_type):
+  events = models.Paste.query.filter_by(poster = event_type).all()
+  return jsonify(json_list = [p.serialize for p in events])
+
+@app.route("/data")
+def data():
+    return api_all()
+
+@app.route("/chart")
+def chart():
+    return render_template("chart.html")
+
+@app.route("/data/dataforchart")
+def data_for_chart():
+    #works 5pm pastes = models.Paste.query.order_by("date desc").all()
+    #works 5pm json_list = [p.serialize for p in pastes]
+    #works 5pm return json.dumps(json_list)
+    pastes = models.Paste.query.order_by("date desc").all()
+    d = dict()
+    for p in pastes:
+        key = p.poster
+        if key in d:
+            d[key][0] += p.value
+            d[key][1] += 1
+        else:
+            d[key] = [p.value, 1]
+    listdict = list()
+    for sec in d.items():
+        x = dict()
+        x['name'] = sec[0]
+        x['tot'] = sec[1][0]
+        x['tot2'] = sec[1][1]
+        listdict.append(x)
+    #for sec in d.items():
+    #    x = dict()
+    #    x['name'] = sec[0]
+    #    x['tot'] = sec[1][1]
+    #    listdict.append(x)
+    return json.dumps(listdict)
+
+@app.route("/oldfaith")
+def old():
+    pastes = models.Paste.query.order_by("date desc").all()
+    json_list = [p.serialize for p in pastes]
+    return json.dumps(json_list)
+
+@app.route("/lotsofdata")
+def people():
+    my_people = {
+        'alice': 25,
+        'bob': 21,
+        'charlie': 20,
+        'doug': 28
+    }
+    return my_people
+
+@app.route("/map")
+def displaymap():
+    return render_template("map.html")
+
+@app.route('/projects/highpoverty/states')
+def high_poverty_states():
+    donors_choose_url = "http://api.donorschoose.org/common/json_feed.html?highLevelPoverty=true&APIKey=DONORSCHOOSE"
+    response = urllib2.urlopen(donors_choose_url)
+    json_response = json.load(response)
+    states = set()
+    for proposal in json_response["proposals"]:
+        states.add(proposal["state"])
+
+    return json.dumps(list(states))
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
--- models.py
+++ models.py
@@ -9,12 +9,14 @@ class Paste(db.Model):
     poster = db.Column(db.String(51))
     paste = db.Column(db.Text())
     date = db.Column(db.DateTime)
+    value = db.Column(db.Integer)
 
-    def __init__(self, text, poster):
+    def __init__(self, text, poster, value):
         self.paste = text
         self.poster = poster
         self.id = str(uuid.uuid4())
         self.date = datetime.datetime.now().strftime("%c")
+        self.value = value
 
     @property
     def serialize(self):
@@ -22,4 +24,5 @@ class Paste(db.Model):
         return {'id': self.id,
     	       'date': self.date.strftime("%c"),
     	       'poster': self.poster,
+               'value': self.value,
                'paster': self.paste }
